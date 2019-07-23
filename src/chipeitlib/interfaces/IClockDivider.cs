5c408e2ad0ff92de0d03212ae3498880de2a23ce
Sergio Luis Para <sergio.l.para@gmail.com>
2019-07-23 20:41:52 +0000

    Document IClockDivider and IClockObserver interfaces

--- src/chipeitlib/interfaces/IClockDivider.cs
+++ src/chipeitlib/interfaces/IClockDivider.cs
@@ -2,8 +2,43 @@ using System;
 
 namespace Chipeit.Lib.Interfaces
 {
+    /// <summary>
+    /// Represents a clock frequency divider. It implements the observer pattern
+    /// to trigger a clock tick on each of its observers.
+    /// </summary>
     public interface IClockDivider
     {
+        /// <summary>
+        /// The number of milliseconds per step of this clock divider.
+        /// This means that every n <see cref="MillisPerStep"/>, the <see cref="IClockDivider"/>
+        /// instance will trigger a step on its <see cref="IClockObserver"/>.
+        /// </summary>
+        long MillisPerStep { get; set; }
 
+        /// <summary>
+        /// Milliseconds left until the next time this <see cref="IClockDivider"/>
+        /// will trigger. Will always be equal to or less than <see cref="MillisPerStep"/>.
+        /// It can even be negative if the last trigger time was missed.
+        /// </summary>
+        long MillisLeft { get; }
+
+        /// <summary>
+        /// Adds a <see cref="IClockObserver"/> to be notified on each step.
+        /// </summary>
+        /// <param name="observer">
+        /// The <see cref="IClockObserver"/> to be added as an observer.
+        /// </param>
+        /// <returns>
+        /// A <see cref="IDisposable"/> representing the subscription.
+        /// </returns>
+        IDisposable AddObserver(IClockObserver observer);
+
+        /// <summary>
+        /// Updates the internal measure of time of this <see cref="IClockDivider"/>.
+        /// After the update, if <see cref="MillisLeft"/> is less than or
+        /// equal to zero, this <see cref="IClockDivider"/> will call to
+        /// <see cref="IClockObserver.OnClockTick"/> on each one of its observers.
+        /// </summary>
+        void TriggerUpdate();
     }
 }
--- src/chipeitlib/interfaces/IClockObserver.cs
+++ src/chipeitlib/interfaces/IClockObserver.cs
@@ -1,9 +1,22 @@
-using System;
-
 namespace Chipeit.Lib.Interfaces
 {
+    /// <summary>
+    /// Represents an object that should do an action on every clock tick.
+    /// On a CHIP-8 emulator, good candidates to be a <see cref="IClockObserver"/>
+    /// are the CPU (which must execute an instruction on each clock tick), the
+    /// sound timer (which must play a sound for a given number of clock ticks),
+    /// and the delay timer (which holds a flag for a given number of clock
+    /// ticks, and it is used to sync events).
+    ///
+    /// For funsies, the CPU clock and the timers' clocks work at different
+    /// frequencies - hence the <see cref="IClockDivider"/> interface.
+    /// </summary>
     public interface IClockObserver
     {
-
+        /// <summary>
+        /// Notifies this <see cref="IClockObserver"/> that the clock that
+        /// synchronizes its actions ticked.
+        /// </summary>
+        void OnClockTick();
     }
 }
