f57b611053aab583b07f3b60ad85a5971e7ec0b4
Sergio Luis Para <sergio.l.para@gmail.com>
2019-07-23 20:08:53 +0000

    Add minor documentation details to Clock implementation

--- src/chipeitlib/core/Clock.cs
+++ src/chipeitlib/core/Clock.cs
@@ -5,6 +5,10 @@ using Chipeit.Lib.Interfaces;
 
 namespace Chipeit.Lib.Core
 {
+    /// <summary>
+    /// Acts as a time reference for the entire system, using the total ceiled
+    /// milliseconds since DateTime.MinValue.
+    /// </summary>
     public class Clock : IClock
     {
         long IClock.Millis => mMillis;
