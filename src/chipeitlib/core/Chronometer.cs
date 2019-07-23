ec2e354fbbf2b5ce54d57f3a81df9819d92a7667
Sergio Luis Para <sergio.l.para@gmail.com>
2019-07-23 20:07:33 +0000

    Document the IChronometer interface and implement a Chronometer class

---
+++ src/chipeitlib/core/Chronometer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+using Chipeit.Lib.Interfaces;
+
+namespace Chipeit.Lib.Core
+{
+    /// <summary>
+    /// Keeps track of how much time has passed using a <see cref="IClock"/> as
+    /// the reference.
+    /// </summary>
+    public class Chronometer : IChronometer
+    {
+        /// <summary>
+        /// Creates a new <see cref="Chronometer"/> instance.
+        /// </summary>
+        /// <param name="clock">
+        /// The <see cref="IClock"/> used as a reference for time measurement.
+        /// </param>
+        public Chronometer(IClock clock)
+        {
+            mClock = clock;
+        }
+
+        bool IChronometer.IsRunning => mbIsRunning;
+
+        long IClock.Millis => mClockMillis - mIdleTime;
+
+        void IChronometer.Start()
+        {
+            lock (mSyncLock)
+            {
+                if (mbIsRunning)
+                    return;
+
+                mClock.Update();
+                mIdleTime += mClock.Millis - mLastStop;
+                mbIsRunning = true; 
+            }
+        }
+
+        void IChronometer.Stop()
+        {
+            lock (mSyncLock)
+            {
+                if (!mbIsRunning)
+                    return;
+
+                mbIsRunning = false;
+                mLastStop = mClock.Millis;
+            }
+        }
+
+        IClock IClock.Update()
+        {
+            lock (mSyncLock)
+            {
+                if (!mbIsRunning)
+                {
+                    throw new InvalidOperationException(
+                        LibLocalization.Get().Localize(
+                            LibLocalization.Keys.ChronometerNotRunningException));
+                }
+
+                mClockMillis = mClock.Update().Millis;
+                return this;
+            }
+        }
+
+        readonly IClock mClock;
+        readonly object mSyncLock;
+
+        long mIdleTime;
+        long mLastStop;
+        long mClockMillis;
+
+        bool mbIsRunning;
+    }
+}
--- src/chipeitlib/interfaces/IChronometer.cs
+++ src/chipeitlib/interfaces/IChronometer.cs
@@ -2,8 +2,36 @@ using System;
 
 namespace Chipeit.Lib.Interfaces
 {
-    public interface IChronometer
+    /// <summary>
+    /// Keeps track of how much time has passed for the whole time it has been
+    /// running.
+    /// </summary>
+    public interface IChronometer : IClock
     {
+        /// <summary>
+        /// Flag indicating wether or not this <see cref="IChronometer"/> is
+        /// running.
+        /// The <see cref="IChronometer"/> is started using the <see cref="Start"/>
+        /// method, and this flag will be <code>true</code>.
+        /// The <see cref="IChronometer"/> is stopped using the <see cref="Stop"/>
+        /// method, and this flag will be <code>false</code>.
+        /// </summary>
+        bool IsRunning { get; }
 
+        /// <summary>
+        /// Starts the chronometer. While the chronometer is running, it will
+        /// update its <see cref="IClock.Millis"/> property. While the
+        /// <see cref="IChronometer"/> instance is started, the <see cref="IsRunning"/>
+        /// property will be <code>true</code>
+        /// </summary>
+        void Start();
+
+        /// <summary>
+        /// Stops the chronometer. While the chronometer is stopped, it will
+        /// not update its <see cref="IClock.Millis"/> property. While the
+        /// <see cref="IChronometer"/> instance is stopped, the <see cref="IsRunning"/>
+        /// property will be <code>false</code>.
+        /// </summary>
+        void Stop();
     }
 }
