2f24d660d0ee020b15cb9a1a2898ef885bae2f60
Sergio Luis Para <sergio.l.para@gmail.com>
2019-07-27 07:32:18 +0000

    Several changes to IClockDivider / IClockObserver
    * Rename IClockObserver to IClockDividerObserver to make the dependency clear.
    * Rename Millis* properties to Ms* to make them shorter while still reasonable.
    * Change IClockDividerObserver.OnClockTick() method to .ClockDividerStep event.

--- src/chipeitlib/interfaces/IClockDivider.cs
+++ src/chipeitlib/interfaces/IClockDivider.cs
@@ -10,35 +10,36 @@ namespace Chipeit.Lib.Interfaces
     {
         /// <summary>
         /// The number of milliseconds per step of this clock divider.
-        /// This means that every n <see cref="MillisPerStep"/>, the <see cref="IClockDivider"/>
-        /// instance will trigger a step on its <see cref="IClockObserver"/>.
+        /// This means that every n <see cref="MsPerStep"/>, the <see cref="IClockDivider"/>
+        /// instance will trigger a step on its <see cref="IClockDividerObserver"/>.
         /// </summary>
-        long MillisPerStep { get; set; }
+        long MsPerStep { get; set; }
 
         /// <summary>
         /// Milliseconds left until the next time this <see cref="IClockDivider"/>
-        /// will trigger. Will always be equal to or less than <see cref="MillisPerStep"/>.
+        /// will trigger. Will always be equal to or less than <see cref="MsPerStep"/>.
         /// It can even be negative if the last trigger time was missed.
         /// </summary>
-        long MillisLeft { get; }
+        long MsLeft { get; }
 
         /// <summary>
-        /// Adds a <see cref="IClockObserver"/> to be notified on each step.
+        /// Adds a <see cref="IClockDividerObserver"/> to be notified on each step.
         /// </summary>
         /// <param name="observer">
-        /// The <see cref="IClockObserver"/> to be added as an observer.
+        /// The <see cref="IClockDividerObserver"/> to be added as an observer.
         /// </param>
         /// <returns>
         /// A <see cref="IDisposable"/> representing the subscription.
         /// </returns>
-        IDisposable AddObserver(IClockObserver observer);
+        IDisposable AddObserver(IClockDividerObserver observer);
 
         /// <summary>
         /// Updates the internal measure of time of this <see cref="IClockDivider"/>.
-        /// After the update, if <see cref="MillisLeft"/> is less than or
-        /// equal to zero, this <see cref="IClockDivider"/> will call to
-        /// <see cref="IClockObserver.OnClockTick"/> on each one of its observers.
+        /// After the update, if <see cref="MsLeft"/> is less than or
+        /// equal to zero, this <see cref="IClockDivider"/> will trigger event
+        /// <see cref="IClockDividerObserver.ClockDividerStep"/> on each one of
+        /// its observers.
         /// </summary>
-        void TriggerUpdate();
+        void Update();
     }
 }
--- src/chipeitlib/interfaces/IClockDividerObserver.cs
+++ src/chipeitlib/interfaces/IClockDividerObserver.cs
@@ -1,8 +1,10 @@
+using System;
+
 namespace Chipeit.Lib.Interfaces
 {
     /// <summary>
     /// Represents an object that should do an action on every clock tick.
-    /// On a CHIP-8 emulator, good candidates to be a <see cref="IClockObserver"/>
+    /// On a CHIP-8 emulator, good candidates to be a <see cref="IClockDividerObserver"/>
     /// are the CPU (which must execute an instruction on each clock tick), the
     /// sound timer (which must play a sound for a given number of clock ticks),
     /// and the delay timer (which holds a flag for a given number of clock
@@ -11,12 +13,12 @@ namespace Chipeit.Lib.Interfaces
     /// For funsies, the CPU clock and the timers' clocks work at different
     /// frequencies - hence the <see cref="IClockDivider"/> interface.
     /// </summary>
-    public interface IClockObserver
+    public interface IClockDividerObserver
     {
         /// <summary>
-        /// Notifies this <see cref="IClockObserver"/> that the clock that
-        /// synchronizes its actions ticked.
+        /// Event triggered when the <see cref="IClockDivider"/> triggers a
+        /// step.
         /// </summary>
-        void OnClockTick();
+        event EventHandler ClockDividerStep;
     }
 }
