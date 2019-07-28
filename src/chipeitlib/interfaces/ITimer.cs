8fdfb3330726033b31a9416762a4bc98a20bdee0
Sergio Luis Para <sergio.l.para@gmail.com>
2019-07-28 16:24:42 +0000

    Specify the ITimer interface

--- src/chipeitlib/interfaces/ITimer.cs
+++ src/chipeitlib/interfaces/ITimer.cs
@@ -2,8 +2,21 @@ using System;
 
 namespace Chipeit.Lib.Interfaces
 {
-    public interface ITimer
+    /// <summary>
+    /// Represents a timer. In Chip-8, a timer counts down from a specified
+    /// number, decreasing said number each clock tick.
+    /// </summary>
+    public interface ITimer : IClockDividerObserver
     {
+        /// <summary>
+        /// Ticks left in the countdown.
+        /// </summary>
+        byte TicksLeft { get; set; }
 
+        /// <summary>
+        /// Indicates wether or not this <see cref="ITimer"/> is active.
+        /// An active timer has one or more <see cref="TicksLeft"/>.
+        /// </summary>
+        bool IsActive { get; }
     }
 }
