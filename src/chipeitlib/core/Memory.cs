ac2ff03a6a4ae4cb5fe2aa5fcc6abe9fd48c9086
Sergio Luis Para <sergio.l.para@gmail.com>
2019-07-28 15:38:51 +0000

    Implement IMemory interface and a generic Memory struct

---
+++ src/chipeitlib/core/Memory.cs
@@ -0,0 +1,27 @@
+﻿using Chipeit.Lib.Interfaces;
+
+namespace Chipeit.Lib.Core
+{
+    public struct Memory<T> : IMemory<T>
+    {
+        T IMemory<T>.this[ulong address]
+        {
+            get => mContent[address];
+            set => mContent[address] = value;
+        }
+
+        ulong IMemory<T>.Size => (ulong)mContent.LongLength;
+
+        public Memory(ulong size)
+        {
+            mContent = new T[size];
+        }
+
+        public Memory(T[] content)
+        {
+            mContent = content;
+        }
+
+        readonly T[] mContent;
+    }
+}
--- src/chipeitlib/interfaces/IMemory.cs
+++ src/chipeitlib/interfaces/IMemory.cs
@@ -1,9 +1,22 @@
-using System;
-
 namespace Chipeit.Lib.Interfaces
 {
-    public interface IMemory
+    /// <summary>
+    /// Represents a R/W memory of type <see cref="T"/> accessible by address.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IMemory<T>
     {
+        /// <summary>
+        /// Size of the memory.
+        /// </summary>
+        ulong Size { get; }
 
+        /// <summary>
+        /// Reads from, or writes to this <see cref="IMemory{T}"/> at the
+        /// specified address.
+        /// </summary>
+        /// <param name="address"></param>
+        /// <returns></returns>
+        T this[ulong address] { get; set; }
     }
 }
