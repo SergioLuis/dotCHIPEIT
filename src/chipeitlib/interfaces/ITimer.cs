785f96dd1d3b050f6e856e0a639c652496da0664
Sergio Luis Para <sergio.l.para@gmail.com>
2019-07-28 22:04:01 +0000

    Add some changes to ITimer interface, specify ITimerObserver one
    Implement (again) the observer pattern - in the original Kotlin CHIPEIT prokect
    the ITimerObserver is actually called a 'ISwitchObserver', which hides its
    intended use. I found it hard to read when coming back to the project after
    a few months (same with IClockDivider - IClockDividerObserver, the later was
    actually called IClockObserver), so the rename was mandatory.

--- src/chipeitlib/interfaces/ITimer.cs
+++ src/chipeitlib/interfaces/ITimer.cs
@@ -18,5 +18,39 @@ namespace Chipeit.Lib.Interfaces
         /// An active timer has one or more <see cref="TicksLeft"/>.
         /// </summary>
         bool IsActive { get; }
+
+        /// <summary>
+        /// Adds a <see cref="ITimerObserver"/> to be notified when the timer
+        /// reachs zero (<code>disabled</code>) and when it becomes different
+        /// than zero (<code>enabled</code>).
+        /// </summary>
+        /// <param name="observer"></param>
+        /// <returns></returns>
+        IDisposable AddObserver(ITimerObserver observer);
+    }
+
+    /// <summary>
+    /// Arguments to the event that <see cref="ITimer"/> emits to
+    /// <see cref="ITimerObserver"/> when the enablement state of the Timer
+    /// changes.
+    /// </summary>
+    public class TimerEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Whether or not the <see cref="ITimer"/> is <code>enabled</code>
+        /// (it's value is different from zero) or <code>disabled</code> (it's
+        /// value is zero).
+        /// </summary>
+        public bool Enabled { get; private set; }
+
+        /// <summary>
+        /// Builds a <see cref="TimerEventArgs"/> from the enablement status
+        /// of the <see cref="ITimer"/>.
+        /// </summary>
+        /// <param name="enabledState"></param>
+        public TimerEventArgs(bool enabledState)
+        {
+            Enabled = enabledState;
+        }
     }
 }
---
+++ src/chipeitlib/interfaces/ITimerObserver.cs
@@ -0,0 +1,23 @@
+﻿namespace Chipeit.Lib.Interfaces
+{
+    /// <summary>
+    /// Represents an object that observes changes in a <see cref="ITimer"/>,
+    /// which can be <code>enabled</code> or <code>disabled</code>, but not
+    /// both nor neither at any given time.
+    /// In a Chip-8 emulator, the perfect candidate to be a <see cref="ITimerObserver"/>
+    /// is a physical speaker or beeper that must emit sound when <code>enabled</code>,
+    /// and be quiet when <code>disabled</code>.
+    /// </summary>
+    public interface ITimerObserver
+    {
+        /// <summary>
+        /// Event handler triggered when the <see cref="ITimer"/> this
+        /// <see cref="ITimerObserver"/> instance is subscribed to changes.
+        /// </summary>
+        /// <param name="sender">
+        /// The <see cref="ITimer"/> that triggered this event.
+        /// </param>
+        /// <param name="e">Arguments of the event</param>
+        void EnablementStatusChange(object sender, TimerEventArgs e);
+    }
+}
