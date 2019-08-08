207494f1dfbd7f1a6ecba8e111ee96c51c9a862f
Sergio Luis Para <sergio.l.para@gmail.com>
2019-08-08 22:46:06 +0000

    Random number generation deserves its own interface as well
    How can you test something random if you can't mock it to make it...
    ...not random at all?

---
+++ src/chipeitlib/core/DefaultRandomNumber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Chipeit.Lib.Interfaces;
+
+namespace Chipeit.Lib.Core
+{
+    /// <summary>
+    /// Default implementation for the IRandomNumber interface.
+    /// </summary>
+    public class DefaultRandomNumber : IRandomNumber
+    {
+        int IRandomNumber.NextInt(int minimum, int maximum)
+        {
+            return mRandom.Next(minimum, maximum + 1);
+        }
+
+        readonly Random mRandom = new Random(Environment.TickCount);
+    }
+}
---
+++ src/chipeitlib/interfaces/IRandomNumber.cs
@@ -0,0 +1,16 @@
+﻿namespace Chipeit.Lib.Interfaces
+{
+    /// <summary>
+    /// Represents a random number generator.
+    /// </summary>
+    public interface IRandomNumber
+    {
+        /// <summary>
+        /// Returns a random integer in the range [minimum, maximum].
+        /// </summary>
+        /// <param name="minimum">Lower bound for the next random integer.</param>
+        /// <param name="maximum">Upper bound for the next random integer.</param>
+        /// <returns></returns>
+        int NextInt(int minimum, int maximum);
+    }
+}
