3cdfd29721763ddb5f21877737876e7121d4b954
Sergio Luis Para <sergio.l.para@gmail.com>
2019-08-08 22:37:51 +0000

    Implement some dumb extension methods on IMemory
    These methods don't serve to any purpose in particular. Just for funsies!

---
+++ src/chipeitlib/extensions/IMemoryExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Chipeit.Lib.Interfaces;
+
+namespace Chipeit.Lib.Extensions
+{
+    /// <summary>
+    /// Extension methods for the <see cref="IMemory{T}"/> implementations.
+    /// </summary>
+    public static class IMemoryExtensions
+    {
+        /// <summary>
+        /// Fills a <see cref="IMemory{T}"/> with a given value.
+        /// </summary>
+        /// <param name="memory">Memory to fill.</param>
+        /// <param name="value">Value to fill the memory with.</param>
+        public static void Fill<T>(this IMemory<T> memory, T value)
+        {
+            memory.ForEach((idx, _) => memory[idx] = value);
+        }
+
+        /// <summary>
+        /// Executes an action for each one of the elements in the <see cref="IMemory{T}"/>.
+        /// </summary>
+        /// <param name="memory">
+        /// Memory to loop exection the action.
+        /// </param>
+        /// <param name="indexAction">
+        /// Action to execute. <see cref="int"/> param is the index, while
+        /// <see cref="T"/> param is the value of the memory at said index.
+        /// </param>
+        public static void ForEach<T>(
+            this IMemory<T> memory, Action<int, T> indexAction)
+        {
+            for (int i = 0; i < memory.Size; i++)
+                indexAction(i, memory[i]);
+        }
+    }
+}
